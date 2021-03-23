650c5b8c6c0abd2828aff5b26a55f16a95d5462b
Jens Reimann <jreimann@redhat.com>
2021-03-23 14:04:20 +0100

    Fix callback of switch

--- src/switch.rs
+++ src/switch.rs
@@ -1,5 +1,6 @@
 use crate::{random_id, Icon};
 use yew::prelude::*;
+use yew::web_sys::HtmlInputElement;
 
 #[derive(Clone, PartialEq, Properties)]
 pub struct Props {
@@ -17,29 +18,46 @@ pub struct Props {
     pub disabled: bool,
 
     #[prop_or_default]
-    pub on_change: Callback<ChangeData>,
+    pub on_change: Callback<bool>,
 
     #[prop_or_default]
     pub aria_label: String,
 }
 
 pub struct Switch {
+    link: ComponentLink<Self>,
     props: Props,
 
     id: String,
+
+    input_ref: NodeRef,
+}
+
+pub enum Msg {
+    Changed,
 }
 
 impl Component for Switch {
-    type Message = ();
+    type Message = Msg;
     type Properties = Props;
 
-    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
         let id = props.id.as_ref().cloned().unwrap_or_else(|| random_id());
-        Self { props, id }
+        Self {
+            props,
+            id,
+            link,
+            input_ref: Default::default(),
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Changed => {
+                self.props.on_change.emit(self.current_state());
+                return false;
+            }
+        }
     }
 
     fn change(&mut self, props: Self::Properties) -> ShouldRender {
@@ -61,13 +79,14 @@ impl Component for Switch {
         html! {
             <label class="pf-c-switch" for=self.id>
                 <input
+                    ref=self.input_ref.clone()
                     class="pf-c-switch__input"
                     type="checkbox"
                     id=self.id
                     aria-label=self.props.aria_label
                     checked=self.props.checked
                     disabled=self.props.disabled
-                    onchange=self.props.on_change.clone()
+                    onchange=self.link.callback(|_|Msg::Changed)
                     />
                 <span class="pf-c-switch__toggle">
                     { if self.props.label.is_none() { html!{
@@ -84,5 +103,14 @@ impl Component for Switch {
                 }} else {html!{}}}
             </label>
         }
+    }
+}
+
+impl Switch {
+    fn current_state(&self) -> bool {
+        self.input_ref
+            .cast::<HtmlInputElement>()
+            .map(|input| input.checked())
+            .unwrap_or_default()
     }
 }
