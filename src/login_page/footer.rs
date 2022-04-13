57829945179d823f754314d870290edc3de9701d
Jens Reimann <jreimann@redhat.com>
2022-04-13 17:50:31 +0200

    feat: allow setting an onclick handler for the login footer button

--- src/login_page/footer.rs
+++ src/login_page/footer.rs
@@ -4,7 +4,9 @@ use yew::prelude::*;
 #[derive(Clone, Debug, PartialEq, Properties)]
 pub struct LoginMainFooterLinkProps {
     #[prop_or_default]
-    pub href: String,
+    pub href: Option<String>,
+    #[prop_or_default]
+    pub onclick: Option<Callback<MouseEvent>>,
     #[prop_or_default]
     pub label: String,
     #[prop_or_default]
@@ -15,25 +17,26 @@ pub struct LoginMainFooterLinkProps {
 
 #[function_component(LoginMainFooterLink)]
 pub fn login_main_footer_link(props: &LoginMainFooterLinkProps) -> Html {
-    let link = html! {
+    let link = html! (
         <a
             class="pf-c-login__main-footer-links-item-link"
             href={props.href.clone()}
+            onclick={props.onclick.clone()}
             target={props.target.clone()}
             aria_label={props.label.clone()}
             >
             { for props.children.iter() }
         </a>
-    };
+    );
 
     if props.label.is_empty() {
         link
     } else {
-        html! {
+        html! (
             <Tooltip text={props.label.clone()}>
                 {link}
             </Tooltip>
-        }
+        )
     }
 }
 
@@ -49,34 +52,26 @@ pub struct LoginMainFooterProps {
 
 #[function_component(LoginMainFooter)]
 pub fn login_main_footer(props: &LoginMainFooterProps) -> Html {
-    html! {
+    html! (
         <footer class="pf-c-login__main-footer">
             { for props.children.iter() }
 
-            { if props.links.len() > 0 {
-                html!{
-                    <ul class="pf-c-login__main-footer-links">
-                    { for props.links.iter().map(|item|{
-                        html!{ <li class="pf-c-login__main-footer-links-item">{item}</li> }
-                    }) }
-                    </ul>
-                }
-            } else {
-                html!{}
-            }}
+            if props.links.len() > 0 {
+                <ul class="pf-c-login__main-footer-links">
+                { for props.links.iter().map(|item|{
+                    html!{ <li class="pf-c-login__main-footer-links-item">{item}</li> }
+                }) }
+                </ul>
+            }
 
-            { if props.band.len() > 0 {
-                html!{
-                    <div class="pf-c-login__main-footer-band">
-                    { for props.band.iter().map(|item|{
-                        html!{ <p class="pf-c-login__main-footer-band-item">{item}</p> }
-                    }) }
-                    </div>
-                }
-            } else {
-                html!{}
-            }}
+            if props.band.len() > 0 {
+                <div class="pf-c-login__main-footer-band">
+                { for props.band.iter().map(|item|{
+                    html!{ <p class="pf-c-login__main-footer-band-item">{item}</p> }
+                }) }
+                </div>
+            }
 
         </footer>
-    }
+    )
 }
