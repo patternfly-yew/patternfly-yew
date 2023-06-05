8a122af3327da6dd543a6508412c1b64d024d01e
Philip Ridout <phill.ridout@gmail.com>
2023-06-05 20:19:27 +0100

    Review and modify Avarat component for PF5

--- src/components/avatar.rs
+++ src/components/avatar.rs
@@ -1,22 +1,77 @@
 //! Avatar grapic
 use yew::prelude::*;
+use crate::Size;
+
+/// Border style of the [`Avatar`] component
+#[derive(Clone, Default, Debug, PartialEq)]
+pub enum AvatarBorder {
+    #[default]
+    None,
+    Dark,
+    Light
+}
+
+impl From<AvatarBorder> for Classes {
+    fn from(value: AvatarBorder) -> Self {
+        classes!(
+            match value {
+                AvatarBorder::None => "",
+                AvatarBorder::Dark => "pf-m-dark",
+                AvatarBorder::Light => "pf-m-light",
+            }
+        )
+    }
+}
+
+/// Size of the [`Border`] component
+#[derive(Clone, Default, Debug, PartialEq)]
+pub enum AvatarSize {
+    #[default]
+    None,
+    Small,
+    Medium,
+    Large,
+    XLarge,
+}
+
+impl From<AvatarSize> for Classes {
+    fn from(value: AvatarSize) -> Self {
+        classes!(
+            match value {
+                AvatarSize::None => Size::None,
+                AvatarSize::Small => Size::Small,
+                AvatarSize::Medium => Size::Medium,
+                AvatarSize::Large => Size::Large,
+                AvatarSize::XLarge => Size::XLarge,
+            }
+        )
+    }
+}
+
 
 /// Properties for [`Avatar`]
 #[derive(Clone, PartialEq, Properties)]
 pub struct AvatarProperties {
+    /// Required Attributes
+    /// The image "alt" text.
+    pub alt: AttrValue,
+
+    #[prop_or_default]
+    pub border: AvatarBorder,
+    #[prop_or_default]
+    pub class: Classes,
+    #[prop_or_default]
+    pub size: AvatarSize,
     /// The source of the image.
     #[prop_or_default]
     pub src: AttrValue,
-    /// The image "alt" text.
-    #[prop_or("Avatar image".into())]
-    pub alt: AttrValue,
 }
 
 /// Avatar component
 ///
 /// > An **avatar** is a visual used to represent a user. It may contain an image or a placeholder graphic.
 ///
-/// See: <https://www.patternfly.org/v4/components/avatar>
+/// See: <https://pf5.patternfly.org/components/avatar>
 ///
 /// ## Properties
 ///
@@ -25,7 +80,14 @@ pub struct AvatarProperties {
 pub fn avatar(props: &AvatarProperties) -> Html {
     html! {
         <img
-            class="pf-v5-c-avatar"
+            class={
+                classes!(
+                    "pf-v5-c-avatar",
+                    props.border.clone(),
+                    props.size.clone(),
+                    props.class.clone()
+                )
+            }
             src={&props.src}
             alt={&props.alt}
         />
--- src/core/size.rs
+++ src/core/size.rs
@@ -1,9 +1,10 @@
 use crate::AsClasses;
-use yew::Classes;
+use yew::prelude::*;
 
 /// Definition for sizes
 #[derive(Clone, PartialEq, Eq, Ord, PartialOrd, Copy, Debug)]
 pub enum Size {
+    None,
     XSmall,
     Small,
     Medium,
@@ -14,17 +15,26 @@ pub enum Size {
     XXXXLarge,
 }
 
+impl From<Size> for Classes {
+    fn from(value: Size) -> Self {
+        classes!(
+            match value {
+                Size::None => "",
+                Size::XSmall => "pf-m-xs",
+                Size::Small => "pf-m-sm",
+                Size::Medium => "pf-m-md",
+                Size::Large => "pf-m-lg",
+                Size::XLarge => "pf-m-xl",
+                Size::XXLarge => "pf-m-2xl",
+                Size::XXXLarge => "pf-m-3xl",
+                Size::XXXXLarge => "pf-m-4xl",
+            }
+        )
+    }
+}
+
 impl AsClasses for Size {
     fn extend_classes(&self, classes: &mut Classes) {
-        classes.push(match self {
-            Size::XSmall => "pf-m-xs",
-            Size::Small => "pf-m-sm",
-            Size::Medium => "pf-m-md",
-            Size::Large => "pf-m-lg",
-            Size::XLarge => "pf-m-xl",
-            Size::XXLarge => "pf-m-2xl",
-            Size::XXXLarge => "pf-m-3xl",
-            Size::XXXXLarge => "pf-m-4xl",
-        });
+        classes.push(*self);
     }
 }
