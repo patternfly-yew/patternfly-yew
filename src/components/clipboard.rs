fbb809d0343e36b7c0aae897def966d12c67fd3b
Philip Ridout <phill.ridout@gmail.com>
2023-06-06 10:58:35 +0100

    Update clipboard component to PF5

--- src/components/clipboard.rs
+++ src/components/clipboard.rs
@@ -163,14 +163,16 @@ impl Component for Clipboard {
                             <>
                             <div class="pf-v5-c-clipboard-copy__group">
                                 { self.expander(ctx) }
-                                <TextInput
-                                    r#ref={self.text_ref.clone()}
-                                    readonly={ctx.props().readonly | self.expanded}
-                                    value={value}
-                                    name={ctx.props().name.clone()}
-                                    id={ctx.props().id.clone()}
-                                    oninput={ctx.link().callback(|_|Msg::Sync)}
-                                />
+                                <div class="pf-v5-c-form-control">
+                                    <TextInput
+                                        r#ref={self.text_ref.clone()}
+                                        readonly={ctx.props().readonly | self.expanded}
+                                        value={value}
+                                        name={ctx.props().name.clone()}
+                                        id={ctx.props().id.clone()}
+                                        oninput={ctx.link().callback(|_|Msg::Sync)}
+                                    />
+                                </div>
                                 <Tooltip text={self.message}>
                                     <Button aria_label="Copy to clipboard" variant={ButtonVariant::Control} icon={Icon::Copy} onclick={ctx.link().callback(|_|Msg::Copy)}/>
                                 </Tooltip>
