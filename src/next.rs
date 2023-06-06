06ba35c88a48aed5fe06f5ca66b471d07aa6ac35
Philip Ridout <phill.ridout@gmail.com>
2023-06-06 10:29:54 +0100

    Promote the next card component. HTML change to match PF5

--- src/next.rs
+++ src/next.rs
@@ -1,6 +1,5 @@
 //! Preview of upcoming functionality
 
-pub use crate::components::card::next::*;
 pub use crate::components::form::next::*;
 pub use crate::components::table::next::*;
 pub use crate::components::toolbar::next::{
