01990fc04583e01d1b0ef6e47320971da403b6d5
Jens Reimann <ctron@dentrassi.de>
2023-06-23 14:18:16 +0200

    fix: adapt to new input group hierarchy

--- src/components/input_group.rs
+++ src/components/input_group.rs
@@ -1,5 +1,6 @@
 //! Input group
 
+use crate::prelude::ChildrenProperties;
 use yew::prelude::*;
 
 /// Properties for [`InputGroup`]
@@ -27,7 +28,7 @@ pub struct InputGroupProperties {
 /// ## Children
 ///
 /// Input groups can have form elements as their children, and also make use of the
-/// [`InputGroupText`] component to amend the input group with additional text or icons.
+/// [`InputGroupItem`] component to amend the input group with additional content.
 #[function_component(InputGroup)]
 pub fn input_group(props: &InputGroupProperties) -> Html {
     let mut class = classes!("pf-v5-c-input-group");
@@ -43,8 +44,18 @@ pub fn input_group(props: &InputGroupProperties) -> Html {
     )
 }
 
+/// Input group text, as child of [`InputGroupItem`]
+#[function_component(InputGroupText)]
+pub fn input_group_text(props: &ChildrenProperties) -> Html {
+    html!(
+        <span class={"pf-v5-c-input-group__text"}>
+            { for props.children.iter() }
+        </span>
+    )
+}
+
 #[derive(Clone, Debug, PartialEq, Properties)]
-pub struct InputGroupTextProperties {
+pub struct InputGroupItemProperties {
     /// The element's ID
     #[prop_or_default]
     pub id: AttrValue,
@@ -53,23 +64,42 @@ pub struct InputGroupTextProperties {
     pub plain: bool,
 
     #[prop_or_default]
+    pub fill: bool,
+
+    #[prop_or_default]
+    pub r#box: bool,
+
+    #[prop_or_default]
+    pub disabled: bool,
+
+    #[prop_or_default]
     pub children: Children,
 }
 
-/// Input group text, as child of [`InputGroup`]
-#[function_component(InputGroupText)]
-pub fn input_group_text(props: &InputGroupTextProperties) -> Html {
+/// Additional item on an input group.
+#[function_component(InputGroupItem)]
+pub fn input_group_item(props: &InputGroupItemProperties) -> Html {
     let mut class = classes!("pf-v5-c-input-group__item");
 
     if props.plain {
         class.push(classes!("pf-m-plain"));
     }
 
+    if props.fill {
+        class.push(classes!("pf-m-fill"));
+    }
+
+    if props.r#box {
+        class.push(classes!("pf-m-box"));
+    }
+
+    if props.disabled {
+        class.push(classes!("pf-m-disabled"));
+    }
+
     html!(
-        <div {class}>
-            <span class={"pf-v5-c-input-group__text"} id={&props.id}>
-                { for props.children.iter() }
-            </span>
+        <div {class} id={&props.id}>
+            { for props.children.iter() }
         </div>
     )
 }
